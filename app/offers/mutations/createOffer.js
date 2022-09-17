797f17f558a46565f6d8193ab3751a4cd3e66656
GByteos <gbyte@gb-tronix.de>
2022-09-17 21:16:04 +0200

    Update createOffer.js
    Every offer now gets automatically the current user assigned as owner.

--- app/offers/mutations/createOffer.js
+++ app/offers/mutations/createOffer.js
@@ -1,15 +1,29 @@
 import { resolver } from "@blitzjs/rpc"
 import db from "db"
 import { z } from "zod"
+import { useSession } from "@blitzjs/auth"
+
 const CreateOffer = z.object({
   name: z.string(),
   description: z.string(),
   link: z.string(),
 })
-export default resolver.pipe(resolver.zod(CreateOffer), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const offer = await db.offer.create({
-    data: input,
-  })
-  return offer
-})
+export default resolver.pipe(
+  resolver.zod(CreateOffer),
+  resolver.authorize(),
+  (input, { session }) => ({
+    ...input,
+    author: {
+      connect: {
+        id: session.userId,
+      },
+    },
+  }),
+  async (input) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const offer = await db.offer.create({
+      data: input,
+    })
+    return offer
+  }
+)
