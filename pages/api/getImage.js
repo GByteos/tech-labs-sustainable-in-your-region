f7210d9704c1a4a809cfa65cdbbf28f40864440e
GByteos <gbyte@gb-tronix.de>
2022-09-30 20:40:40 +0200

    added support for more image types

--- pages/api/getImage.js
+++ pages/api/getImage.js
@@ -5,22 +5,47 @@ import nextConnect from "next-connect"
 const getImage = nextConnect({
   onError(error, req, res) {
     res.status(501).json({ error: `Sorry something Happened! ${error.message}` })
+    res.end()
   },
   onNoMatch(req, res) {
     // handle invalid HTTP method requests
     res.status(405).json({ error: `Method '${req.method}' not allowed` })
+    res.end()
   },
 })
 
 getImage.get(async (req, res) => {
-  console.log(req.query.imageId)
-
   if (req.query.imageId === undefined) {
     throw new Error("No image id was specified")
   } else {
-    res.setHeader("Content-Type", "image/jpg")
+    const imageNameParts = req.query.imageId.split(".")
+    let contentType
+    switch (imageNameParts[imageNameParts.length - 1].toLowerCase()) {
+      case "jpg":
+        contentType = "image/jpg"
+        break
+      case "gif":
+        contentType = "image/gif"
+        break
+      case "jpeg":
+        contentType = "image/jpeg"
+        break
+      case "png":
+        contentType = "image/png"
+        break
+      case "tiff":
+        contentType = "image/tiff"
+        break
+      case "svg":
+        contentType = "image/svg+xml"
+        break
 
-    const filePath = path.resolve("./public/uploads/", req.query.imageId)
+      default:
+        throw new Error("Unspported file type")
+    }
+    res.setHeader("Content-Type", contentType)
+
+    const filePath = path.resolve(process.env.IMAGE_UPLOAD_URL, req.query.imageId)
     const imageBuffer = fs.createReadStream(filePath)
 
     await new Promise(function (resolve) {
