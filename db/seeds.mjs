dbbbe72680338344201de5e7e9479bdf2aa79a42
GByteos <gbyte@gb-tronix.de>
2022-10-06 20:58:08 +0200

    removed debug message

--- db/seeds.mjs
+++ db/seeds.mjs
@@ -72,36 +72,6 @@ const seed = async () => {
 
   // seed some offers
   console.log(seededData)
-
-  console.log(
-    faker.helpers.arrayElements(
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-        { id: 4 },
-        { id: 5 },
-        { id: 6 },
-        { id: 7 },
-        { id: 8 },
-        { id: 9 },
-        { id: 10 },
-        { id: 11 },
-        { id: 12 },
-        { id: 13 },
-        { id: 14 },
-        { id: 15 },
-        { id: 16 },
-        { id: 17 },
-        { id: 18 },
-        { id: 19 },
-        { id: 20 },
-        { id: 21 },
-      ],
-      3
-    )
-  )
-
   for (let i = 0; i < 500; i++) {
     seededData = await db.offer.create({
       data: {
