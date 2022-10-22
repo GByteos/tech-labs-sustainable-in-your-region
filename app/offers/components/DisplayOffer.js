8008344ab6a59c731fe2d6341e8609abd31057a6
GByteos <gbyte@gb-tronix.de>
2022-10-22 22:51:21 +0200

    Changed overflow behaviour

--- app/offers/components/DisplayOffer.js
+++ app/offers/components/DisplayOffer.js
@@ -4,9 +4,8 @@ import YL from "public/yourlogo.png"
 import { Routes } from "@blitzjs/next"
 import Truncate from "react-truncate"
 
-
 function Logo({ offer }) {
-   if (!offer.logo) {
+  if (!offer.logo) {
     return (
       <>
         <Image className="displaylogo" src={YL} alt="Offer Logo" width="150px" height="150px" />
@@ -29,13 +28,16 @@ function Logo({ offer }) {
 function DisplayOffer({ offer }) {
   // const tags = {
   //   offer.id: useQuery(getPublicTags, "")[0]}
-  
+
   return (
     <>
       <p className="smallheader1">
         {offer.offerType} -{" "}
         {offer.offerTags.map((tagname) => (
-          <p key={tagname.id} className="Tags"> {tagname.name} </p>
+          <p key={tagname.id} className="Tags">
+            {" "}
+            {tagname.name}{" "}
+          </p>
         ))}
       </p>
 
@@ -58,13 +60,7 @@ function DisplayOffer({ offer }) {
             </a>
           </Link>
 
-          <Truncate
-            lines={1}
-            width={1200} // width being how much you want to truncate your copy
-            ellipsis="&hellip;"
-          >
-            <p>{offer.description}</p>
-          </Truncate>
+          <p>{offer.description}</p>
         </div>
         <div className="para_main info">{offer.openingTimes}</div>
       </article>
